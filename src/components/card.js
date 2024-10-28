a7d2d01749241c0c3bb77668ddf590e368a332e7
AmitTiparadi <140626232+AmitTiparadi@users.noreply.github.com>
2024-10-28 16:50:20 +0530

    Add files via upload

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,23 @@
+import React from 'react';
 
+class Cards extends React.Component {
+  render() {
+    const { image, title, text } = this.props;
+    return (
+      <div className="card-group">
+        <div className="card">
+          <img src={image} className={text} alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{text}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cards;
